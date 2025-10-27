77b5f6583687ffd91700bdc5f72c872e39e08841
Reuel Yangad <reueljayesh.yangad@calbaptist.edu>
2025-10-27 09:39:15 -0700

    Tests for AdvancedTickets

--- tst/TicketTest.java
+++ tst/TicketTest.java
@@ -9,6 +9,13 @@ public class TicketTest {
         Ticket ticket2 = new WalkupTicket(5);
         System.out.println(ticket);
 
+        Ticket ticket3 = new AdvancedTicket(15.5, 4, 6);
+        System.out.println(ticket3);
 
+        Ticket ticket4 = new AdvancedTicket(20.5, 4);
+        System.out.println(ticket4);
+
+        Ticket ticket5 = new AdvancedTicket(4, 4);
+        System.out.println(ticket5);
     }
 }
