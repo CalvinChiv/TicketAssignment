7e94f0d99427946f6515cd11dd87a5e20722343f
Reuel Yangad <reueljayesh.yangad@calbaptist.edu>
2025-10-27 09:32:59 -0700

    Tests for WalkUp Tickets

--- tst/TicketTest.java
+++ tst/TicketTest.java
@@ -1,5 +1,14 @@
 public class TicketTest {
     public static void main(String[] args) {
+        Ticket ticket = new WalkupTicket(1.5, 2);
+        System.out.println(ticket);
+
+        Ticket ticket1 = new WalkupTicket(2.0);
+        System.out.println(ticket);
+
+        Ticket ticket2 = new WalkupTicket(5);
+        System.out.println(ticket);
+
 
     }
 }
